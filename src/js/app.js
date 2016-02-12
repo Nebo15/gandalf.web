cb20d0d102a9d03d1a58363ffd2c8a17e0ea5717
Alexey Bondarenko <alexeybondarenko@me.com>
2016-02-12 20:02:56 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,25 @@ angular.module('app', [
   'ng-gandalf'
 ]);
 
+angular.module('app').service('Store', function ($sessionStorage, $gandlaf) {
+
+  var storage = $sessionStorage.$default({
+    'rules': [],
+    'conditions': []
+  });
+
+  this.fetch = function () {
+    return $gandlaf.get().then(function (resp) {
+      storage.conditions = resp.fields;
+      storage.rules = resp.rules;
+      return storage;
+    });
+  };
+  this.get = function () {
+    return storage;
+  };
+});
+
 angular.module('app').controller('MainController', function ($scope, $gandlaf, Condition, Rule) {
 
   $scope.conditions = [];
