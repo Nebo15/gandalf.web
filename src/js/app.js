4c88162aff52084f3066f2afa548e9dcabf6ba2f
Alexey Bondarenko <alexeybondarenko@me.com>
2016-02-12 19:09:24 +0200

    base

--- src/js/app.js
+++ src/js/app.js
@@ -2,72 +2,32 @@
 
 
 angular.module('app', [
-  'scoring'
+  'ng-gandalf'
 ]);
 
-angular.module('scoring', []).service('$scoring', function ($http, Condition, Rule) {
-
-  this.get = function () {
-    return $http.get('data/api.json').then(function (resp) {
-      return resp.data;
-    }).then(function (data) {
-      data.fields = data.fields.map(function (item) {
-        return new Condition(item);
-      });
-      data.rules = data.rules.map(function (item) {
-        return new Rule(item);
-      });
-      return data;
-    })
-  }
-}).factory('Condition', function () {
-
-  function Condition (obj) {
-    var options = obj ? angular.copy(obj) : {};
-
-    this.name = options.name;
-    this.type = options.type;
-    this.title = options.title;
-    this.source = options.source;
-  }
-
-  return Condition;
-}).factory('Rule', function () {
-
-  function Rule (obj) {
-    var options = obj ? angular.copy(obj) : {};
-
-    this.id = options.id;
-    this.priority = options.priority;
-    this.result = options.result;
-    this.conditions = (options.conditions || []).map(function (item) {
-      return new RuleCondition(item);
-    });
-  }
-
-  function RuleCondition (obj) {
-    var options = obj ? angular.copy(obj) : {};
-
-    this.field_name = options.field_name;
-    this.condition = options.condition;
-    this.value = options.value;
-  }
-  return Rule;
-});
-
-
-angular.module('app').controller('MainController', function ($scope, $scoring, Condition) {
+angular.module('app').controller('MainController', function ($scope, $gandlaf, Condition, Rule) {
 
   $scope.conditions = [];
   $scope.rules = [];
 
-  $scoring.get().then(function (resp) {
+  $gandlaf.get().then(function (resp) {
     $scope.conditions = resp.fields;
     $scope.rules = resp.rules;
   });
 
   $scope.addNewCondition = function () {
-    $scope.conditions.push(new Condition());
+    var newCondition = new Condition({
+      title: 'new name'
+    });
+    $scope.conditions.push(newCondition);
+    $scope.rules.forEach(function (item) {
+      item.addCondition(newCondition);
+    });
+  };
+  $scope.addNewRule = function () {
+    $scope.rules.push(Rule.fromConditions($scope.conditions, {
+      priority: $scope.rules.length
+    }));
   }
 
 });
---
+++ src/js/modules/gandalf.js
@@ -0,0 +1,64 @@
+'use strict';
+
+angular.module('ng-gandalf', []).service('$gandlaf', function ($http, Condition, Rule) {
+
+  this.get = function () {
+    return $http.get('data/api.json').then(function (resp) {
+      return resp.data;
+    }).then(function (data) {
+      data.fields = data.fields.map(function (item) {
+        return new Condition(item);
+      });
+      data.rules = data.rules.map(function (item) {
+        return new Rule(item);
+      });
+      return data;
+    })
+  }
+}).factory('Condition', function () {
+
+  function Condition (obj) {
+    var options = obj ? angular.copy(obj) : {};
+
+    this.name = options.name;
+    this.type = options.type;
+    this.title = options.title;
+    this.source = options.source;
+  }
+
+  return Condition;
+}).factory('Rule', function () {
+
+  function Rule (obj) {
+    var options = obj ? angular.copy(obj) : {};
+
+    this.id = options.id;
+    this.priority = options.priority;
+    this.result = options.result;
+    this.conditions = (options.conditions || []).map(function (item) {
+      return new RuleCondition(item);
+    });
+  }
+  Rule.prototype.addCondition = function (condition) {
+    this.conditions.push(new RuleCondition({
+      name: condition.name
+    }))
+  };
+
+  Rule.fromConditions = function (conditions, options) {
+    var rule = new Rule(options);
+    conditions.forEach(function (item) {
+      rule.addCondition(item);
+    });
+    return rule;
+  };
+
+  function RuleCondition (obj) {
+    var options = obj ? angular.copy(obj) : {};
+
+    this.field_name = options.field_name;
+    this.condition = options.condition;
+    this.value = options.value;
+  }
+  return Rule;
+});
