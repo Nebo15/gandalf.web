42266154f2090fdc2813ecd305fc0186dfc59e6e
Alexey Bondarenko <alexeybondarenko@me.com>
2016-02-23 17:16:55 +0200

    curl example

--- src/js/controllers/decision-details-controller.js
+++ src/js/controllers/decision-details-controller.js
@@ -1,4 +1,3 @@
-
 angular.module('app').controller('DecisionDetailsController', function ($scope, $uibModal, $timeout, decision, DecisionRule) {
 
   var table = decision;
@@ -9,6 +8,27 @@ angular.module('app').controller('DecisionDetailsController', function ($scope, 
     handle: '> .decision-table__handler'
   };
 
+  $scope.getSampleCheck = function (table) {
+    var example = {};
+    table.fields.forEach(function (item) {
+      switch (item.type) {
+        case 'number':
+          example[item.alias] = 100;
+          break;
+        case 'string':
+          example[item.alias] = 'sample';
+          break;
+        case 'bool':
+          example[item.alias] = true;
+          break;
+        case 'default':
+          example[item.alias] = 'sample';
+          break;
+      }
+    });
+
+    return JSON.stringify(example);
+  }
   $scope.addNewField = function () {
 
     var modalInstance = $uibModal.open({
