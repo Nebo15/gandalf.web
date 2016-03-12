9b0dcdf78633db4bf3dd09dcf3bfa2a269e20b57
Alexey Bondarenko <alexeybondarenko@me.com>
2016-03-12 16:15:40 +0200

    move decision input to directive

--- src/js/directives/components/decision-input.js
+++ src/js/directives/components/decision-input.js
@@ -4,22 +4,24 @@ angular.module('app').directive('decisionInput', function () {
   return {
     restrict: 'E',
     scope: {
-      type: '='
+      type: '=',
+      decisions: '=',
+      model: '=model'
     },
     templateUrl: 'templates/directives/decision-input.html',
-    controller: 'DecisionInputController'
+    link: function (scope, el, attrs, ctrl) {
+      scope.getDecisions = function (current) {
+        var statesCopy = scope.decisions.slice(0);
+        if (current) {
+          statesCopy.unshift(current);
+        }
+        return statesCopy;
+      };
+      scope.onSelect = function ($item) {
+        if (scope.decisions.indexOf($item) === -1) {
+          scope.decisions.push($item);
+        }
+      }
+    }
   };
-}).service('DecisionInputService', function (_) {
-
-  var decisions = [];
-  this.addDecision = function (decision) {
-    decisions = _.unique(decision);
-  };
-  this.getDecisions = function () {
-    return decisions;
-  }
-}).controller('DecisionInputController', function ($scope, DecisionInputService) {
-
-  $scope.decisions = DecisionInputService.getDecisions();
-
 });
