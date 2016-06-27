d14d357b611b0fae7ddb2bc6b8243524fea80378
Alexey Bondarenko <alexeybondarenko@me.com>
2016-06-27 17:12:28 +0300

    user edit

--- src/js/modules/gandalf/models/user.js
+++ src/js/modules/gandalf/models/user.js
@@ -4,7 +4,7 @@ angular.module('ng-gandalf').factory('User', function ($gandalf) {
     var obj = data || {};
     this.id = obj._id;
     this.username = obj.username;
-    this.admin = obj.admin;
+    this.active = obj.active;
 
     this.firstName = obj.first_name;
     this.lastName = obj.last_name;
@@ -31,9 +31,20 @@ angular.module('ng-gandalf').factory('User', function ($gandalf) {
     });
   };
 
+  User.prototype.update = function (password) {
+    var self = this;
+    return $gandalf.admin.updateUser({
+      first_name: this.firstName,
+      last_name: this.lastName,
+      email: this.email
+    }, password).then(function (resp) {
+      self.constructor(resp.data);
+      return self;
+    })
+  };
   User.prototype.getName = function () {
     return [this.firstName, this.lastName].filter(function (item) { return item;}).join(' ') || this.username;
-  }
+  };
 
   User.prototype.toJSON = function () {
     return {
@@ -41,6 +52,8 @@ angular.module('ng-gandalf').factory('User', function ($gandalf) {
       username: this.username,
       email: this.email,
       temporary_email: this.temporaryEmail,
+
+      active: this.active,
 
       description: this.description,
 
@@ -54,6 +67,9 @@ angular.module('ng-gandalf').factory('User', function ($gandalf) {
       created_at: this.createdAt,
       updated_at: this.updatedAt
     };
+  };
+  User.prototype.copy = function () {
+    return new User(this.toJSON());
   };
 
   return User;
