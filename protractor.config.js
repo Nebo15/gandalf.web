9341cece7b8ea8efe3cc513a28fae1d946e6ee5d
Alexey Bondarenko <alexeybondarenko@me.com>
2016-06-29 15:42:39 +0300

    add tests

---
+++ protractor.config.js
@@ -0,0 +1,24 @@
+
+exports.config = {
+
+    specs: ['tests/tests/*.js'],
+
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    capabilities: {
+        'shardTestFiles': true,
+        'maxInstances': 1,
+        'browserName': 'chrome'
+    },
+
+    framework: 'jasmine2',
+
+    // Options to be passed to Jasmine.
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    },
+    plugins: [{
+        package: 'protractor-console',
+        logLevels: ['severe']
+    }]
+};
