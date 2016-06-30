d4f7f16fc770984b970f04a74899206249069294
Alexey Bondarenko <alexeybondarenko@me.com>
2016-06-30 18:41:51 +0300

    update tests

--- tests/pages/welcome_page.js
+++ tests/pages/welcome_page.js
@@ -5,8 +5,8 @@ require("../pages/new_project_page.js");
 var welcome_page = function () {
 
     this.clickContinue = function () {
-        browser.isElementPresent(by.className('btn btn-primary'));
-        element(by.className('btn btn-primary')).click();
+        element(by.className('btn btn-submit')).isDisplayed();
+        element(by.className('btn btn-submit')).click();
         return require("./new_project_page.js");
     };
 };
