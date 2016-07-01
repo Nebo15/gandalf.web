6126386c842430d6185068500967688c0344b3ad
Alexey Bondarenko <alexeybondarenko@me.com>
2016-07-01 16:52:32 +0300

    fix transformations

--- src/js/modules/gandalf/constants/transforms.js
+++ src/js/modules/gandalf/constants/transforms.js
@@ -1,42 +1,43 @@
+(function () {
 
-angular.module('ng-gandalf').constant('GANDALF_TRANSFORMS', {
-  matchingType: {
-    scoring: {
-      decisionType: 'numeric',
-      transformFn: function (val) {
-        val = Number(val);
-        return isNaN(val) ? 0 : val;
-      }
-    },
-    decision: {
-      decisionType: 'alpha_num',
-      transformFn: function (val) {
-        return '' + (val || '');
-      }
-    }
-  },
-  decisionType: {
-    string: {
-      transformFn: function (val) {
-        return '' + (val || '');
-      }
-    },
-    numeric: {
-      transformFn: function (val) {
-        val = Number(val);
-        return isNaN(val) ? 0 : val;
-      }
-    },
-    alpha_num: {
-      transformFn: function (val) {
-        var reg = /[a-zA-Z0-9_-]+/gmi;
-        return (('' + val).match(reg) || []).join('')
-      }
-    },
-    json: {
-      transformFn: function (val) {
-        return JSON.parse(JSON.stringify(val));
-      }
-    }
+  function toString (val) {
+    return typeof val !== 'undefined' ? ('' + val) : '';
   }
-});
+  function toNumber (val) {
+    val = Number(val);
+    return isNaN(val) ? 0 : val;
+  }
+
+  angular.module('ng-gandalf').constant('GANDALF_TRANSFORMS', {
+    matchingType: {
+      scoring: {
+        decisionType: 'numeric',
+        transformFn: toNumber
+      },
+      decision: {
+        decisionType: 'alpha_num',
+        transformFn: toString
+      }
+    },
+    decisionType: {
+      string: {
+        transformFn: toString
+      },
+      numeric: {
+        transformFn: toNumber
+      },
+      alpha_num: {
+        transformFn: function (val) {
+          var reg = /[a-zA-Z0-9_-]+/gmi;
+          return (('' + val).match(reg) || []).join('')
+        }
+      },
+      json: {
+        transformFn: function (val) {
+          return JSON.parse(JSON.stringify(val));
+        }
+      }
+    }
+  });
+
+})();
