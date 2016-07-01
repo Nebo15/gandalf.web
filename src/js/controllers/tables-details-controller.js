d1c98ce0989cd997e099aa66d50d656bd3d7f863
Alexey Bondarenko <alexeybondarenko@me.com>
2016-07-01 23:10:44 +0300

    simple and complex view

--- src/js/controllers/tables-details-controller.js
+++ src/js/controllers/tables-details-controller.js
@@ -1,8 +1,51 @@
 
-angular.module('app').controller('TablesDetailsController', function ($scope, $state, table) {
+angular.module('app').controller('TablesDetailsController', function ($scope, $uibModal, $state, table, variant) {
 
   $scope.table = table;
-  if ($scope.table.variants.length == 1) {
-    $scope.variant = $scope.table.variants[0];
-  }
+  $scope.variant = variant;
+
+  $scope.selectVariant = function (variant) {
+    $scope.variant = variant;
+    $state.go($state.current.name, {
+      id: table.id,
+      variantId: $scope.variant.id
+    }, {
+      notify: true,
+      reload: true,
+      location: 'replace'
+    });
+  };
+
+  $scope.createNewVariant = function () {
+    var newVariant = $scope.table.createVariant(variant.id);
+    newVariant.title = $scope.table.title + ' ' + ($scope.table.variants.length+1);
+
+    $scope.table.variants.push(newVariant);
+    $scope.table.save().then(function (table) {
+      return $state.go('tables-details.edit', {
+        table: table.id,
+        variantId: table.variants[table.variants.length - 1].id
+      })
+    })
+  };
+
+  $scope.deleteTable = function (table) {
+    var modalInstance = $uibModal.open({
+      templateUrl: 'templates/modal/delete-table.html',
+      controller: 'DeleteTableController',
+      resolve: {
+        table: table
+      }
+    });
+
+    modalInstance.result.then(function () {
+      table.delete().then(function () {
+        $state.go('tables-list');
+      });
+    });
+  };
+
+  if (!$scope.variant) $scope.selectVariant($scope.table.variants[0]);
+
+
 });
