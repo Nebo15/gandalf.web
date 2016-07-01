f504688c17234e24d8738fb843b4ffb60cfbe629
Alexey Bondarenko <alexeybondarenko@me.com>
2016-07-01 18:35:27 +0300

    navigation for single variant

--- src/js/controllers/tables-details-controller.js
+++ src/js/controllers/tables-details-controller.js
@@ -2,5 +2,7 @@
 angular.module('app').controller('TablesDetailsController', function ($scope, $state, table) {
 
   $scope.table = table;
-
+  if ($scope.table.variants.length == 1) {
+    $scope.variant = $scope.table.variants[0];
+  }
 });
