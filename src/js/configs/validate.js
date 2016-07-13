7ab12eb18a1f324fb69c8693b18ff117b538080a
alex <alex@MacBook-Pro-alex.local>
2016-07-13 14:00:49 +0300

    Validate JSON and save decision on blur

--- src/js/configs/validate.js
+++ src/js/configs/validate.js
@@ -3,4 +3,12 @@
 angular.module('app').config(function($validateProvider) {
 
   $validateProvider.add('alphadashed', /^[a-zA-Z0-9_-]+$/);
+  $validateProvider.add('json', function (value) {
+    try {
+      JSON.parse(value);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  });
 });
